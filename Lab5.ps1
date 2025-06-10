e0eb608b966c919b72a85abca3fdae03216030a4
Mq20005 <michalklaczek05@gmail.com>
2025-06-10 03:38:55 +0200

    Add files via upload
    Tworzenie pliku
    Zmiana nazwy pliku
    Suma kontrolne/hash pliku
    Uruchomienie Notepad
    Wyswietlenie listy plikow posortowana od najkrotszej nazwy
    Przypisywanie do zmiennej obecenj sciezki
    Zapisywanie wartosci zmiennych do pliku
    Wyswietlanie z listy procesow pierwsze 5 tylko z kolumnami nazwa i id
    Pierwsze 5 procesow z najwyzszym zuzyciem pamieci
    Pierwsze 5 procesow z najnizszym zuzyciem pamieci, nazwa kolumny WS->MemoryUsage, kolumy Ws i Nazwy

---
+++ Lab5.ps1
@@ -0,0 +1,11 @@
+﻿New-Item -Name "Nowyplik.txt" -ItemType File #Tworzenie nowgo pustego pliku
+Rename-Item -Path "Nowyplik.txt" -NewName "Nowszyplik.txt" #Zmienianie nazwy stworzonego pliku
+Get-FileHash -Path "Nowszyplik.txt" #Sprawdzanie hashu pliku (sumy kontrolnej)
+notepad #Uruchamianie notatnika z poziomu powershella
+Get-ChildItem -File | Sort-Object {$_.Name.Length} | Select-Object Name #Sortowanie plikow z domysnego katalogu po dlugosci ich nazwy
+$lokalizacja = (pwd).Path #Zapisywanie obecnej lokalizacji o zmiennej
+$lokalizacja >> "Nowszyplik.txt" #Wpisywanie informacji zapisanych w zmiennej do pliku
+Get-Process | Select-Object -First 5 | Select-Object -Property ProcessName, Id #Wyswietlanie pierwszych 5 wierszy, tylko z kolumnami name i id
+Get-Process | Sort-Object -Property WorkingSet -Descending | Select-Object -First 5 #Wyswietlanie 5 procesow z najwiekszym zuzyciem pamieci
+Get-Process | Sort-Object -Property WorkingSet | Select-Object -First 5 ProcessName, @{Name = "MemoryUsage"; Expression = {$_.WorkingSet}} # 5 procesow 
+#zuzywajacych najmniej pamieci, tylko kolumny nazwy i pamieci, zamienona nazwa kolumny z pamiecia
